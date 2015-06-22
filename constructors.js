149268a9d15007509f6c4fee9ae88542556c81a0
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-06-22 11:07:08 -0400

    prototyping

--- constructors.js
+++ constructors.js
@@ -8,31 +8,33 @@ function Character(options) {
   this.name = options.name;
   this.life = 100;
   this.weapon = sword;
-  this.getWeapon = function (weapon, power) {
-    this.weapon = new Weapon ({name: name, power: power})
-  }
-  this.attack = function (monster, weapon){
-    var chance = Math.floor(Math.random() * 10);
-    var critical = Math.floor(Math.random() * 10);
-    if (chance > 1 && critical < 1) {
-      var criticalHit = weapon.power + 5;
-      console.log("Critical Hit!! You hit the monster for " + criticalHit + " damage!");
-      monster.life = monster.life - (weapon.power + 5);
-    }
-    else if (chance > 1) {
-      console.log("You hit the monster for " + weapon.power + " damage!");
-      console.log(weapon.power);
-      monster.life = monster.life - weapon.power;
-    }
-    if (monster.life <= 0) {
-      console.log("You killed zee moster!");
-    }
-  }
-  this.heal = function (lifeAdd) {
-    this.life = lifeAdd.heart + character.life
-  }
 }
 
+Character.prototype.attack = function (monster, weapon){
+  var chance = Math.floor(Math.random() * 10);
+  var critical = Math.floor(Math.random() * 10);
+  if (chance > 1 && critical < 1) {
+    var criticalHit = weapon.power + 5;
+    console.log("Critical Hit!! You hit the monster for " + criticalHit + " damage!");
+    monster.life = monster.life - (weapon.power + 5);
+  }
+  else if (chance > 1) {
+    console.log("You hit the monster for " + weapon.power + " damage!");
+    console.log(weapon.power);
+    monster.life = monster.life - weapon.power;
+  }
+  if (monster.life <= 0) {
+    console.log("You killed zee moster!");
+  }
+};
+
+Character.prototype.heal = function (lifeAdd) {
+  this.life = lifeAdd.heart + character.life
+};
+
+Character.prototype.getWeapon = function (weapon, power) {
+  this.weapon = new Weapon ({name: name, power: power})
+}
 
 //////////////////
 // Main Monster //
@@ -49,32 +51,33 @@ function Monster(options) {
       randomLife = 15;
     }
   this.life = 5 + randomLife
-  this.getWeapon = function (power) {
-    var difficulty = Math.floor(Math.random() * 10);
-    if (difficulty > 1) {
-      this.power = 5;
-    } else {
-      this.power = 10;
-    }
+}
+
+Monster.prototype.attack = function (character, monsterAttack){
+  var chance = Math.floor(Math.random() * 11);
+  var critical = Math.floor(Math.random() * 11);
+  if (character.life <= 0) {
+    console.log("Game Over! You were killed!");
   }
-  this.attack = function (character, monsterAttack){
-    var chance = Math.floor(Math.random() * 11);
-    var critical = Math.floor(Math.random() * 11);
-    if (character.life <= 0) {
-      console.log("Game Over! You were killed!");
-    }
-    if (chance > 1 && critical < 1) {
-      var criticalHit = monsterAttack.power + 5;
-      console.log("Critical Hit!! You were hit for " + criticalHit + " damage!");
-      character.life = character.life - (monsterAttack.power + 5);
-    }
-    if (chance > 1) {
-      console.log("You were hit for " + monsterAttack.power + " damage!");
-      character.life = character.life - monsterAttack.power;
-    }
+  if (chance > 1 && critical < 1) {
+    var criticalHit = monsterAttack.power + 5;
+    console.log("Critical Hit!! You were hit for " + criticalHit + " damage!");
+    character.life = character.life - (monsterAttack.power + 5);
+  }
+  if (chance > 1) {
+    console.log("You were hit for " + monsterAttack.power + " damage!");
+    character.life = character.life - monsterAttack.power;
   }
 }
 
+Monster.prototype.getWeapon = function (power) {
+  var difficulty = Math.floor(Math.random() * 10);
+  if (difficulty > 1) {
+    this.power = 5;
+  } else {
+    this.power = 10;
+  }
+}
 
 /////////////////////
 // Weapons / Items //
